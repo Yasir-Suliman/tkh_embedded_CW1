dac579974c083912cd13b4c897c8c223293c4739
yasir <ys2101327@tkh.edu.eg>
2022-10-29 19:44:26 +0200

    added comments to main.io

--- main.ino
+++ main.ino
@@ -10,23 +10,23 @@ void init();
 int main(){
   init();
 
-  while(1)
+  while(1) // endless loop
   {
-    int pot_data = ADC_read(0b00000101);
-    int button_press = ADC_read(0b00000011);
+    int pot_data = ADC_read(0b00000101); // read potentiometer connected on channel 5
+    int button_press = ADC_read(0b00000011); // read button connected on channel 3
     
     // Serial.print("POT: ");
     // Serial.println(pot_data);
 
-    if(button_press != 0){
-      DIO_SetPinState('d', 3, 'h');      
-    } else{
-      DIO_SetPinState('d', 3, 'l');       
+    if(button_press != 0){ // runs when button is pressed
+      DIO_SetPinState('d', 3, 'h'); // setting pin PD3 as high      
+    } else{ // runs when button not pressed
+      DIO_SetPinState('d', 3, 'l'); // setting pin PD3 as low       
     }
     Serial.print("Button: ");
     Serial.println(button_press);
 
-    if(pot_data > 512){
+    if(pot_data > 512){ //
       DIO_SetPinState('d', 4, 'h');
       DIO_SetPinState('d', 5, 'h');
       DIO_SetPinState('d', 6, 'h');
@@ -40,7 +40,7 @@ int main(){
       DIO_SetPinState('d', 7, 'l');
       Serial.println(pot_data);      
     }
-    char pot_char[16];
+    char pot_char[16]; // empty string
     // Serial.println(pot_char);
     // itoa(pot_data, pot_char, 10);
     // send_char_uart(pot_char);
@@ -96,13 +96,14 @@ int main(){
     //   Serial.println("LEDS: 1111");      
     // }
     
-    _delay_ms(200);
+    _delay_ms(200); // delay for 200ms
   }
 }
 
 void init(){
-  ADC_init();
+  ADC_init(); // Initialize adc
   Serial.begin(9600);
+  //set pins PD3 - PD7 as output
   DIO_SetPinDirection('d', 3, 'o');
   DIO_SetPinDirection('d', 7, 'o');
   DIO_SetPinDirection('d', 6, 'o');
